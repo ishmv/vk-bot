103ce897aa08f5ef68349ac9521ac62dea5a1b62
Leon Ishimov <leonty6463@gmail.com>
2017-04-02 16:06:22 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,6 +2,6 @@
 ini_set('display_errors', 1);
 error_reporting(0);
 //Строка для подтверждения адреса сервера из настроек Callback API
-$config['confirmation_token'] = 'cb4dc639'; // [ https://vk.com/{id_group}?act=api ]
-$config['token'] = '28e86e22fd433acd92df73fd787dabe04c7e5d61f0611ff28bff6f1151a9735fdb6b8c73376292490d1c1'; // [ https://vk.com/{id_group}?act=tokens ];
+$config['confirmation_token'] = 'токен'; // [ https://vk.com/{id_group}?act=api ]
+$config['token'] = 'токен'; // [ https://vk.com/{id_group}?act=tokens ];
 ?>
